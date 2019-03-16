50046fc6471398c588cb632a5e79dd2276c2a5ba
Ryan Szalanski <44514729+rszalanski@users.noreply.github.com>
2019-03-15 20:26:52 -0700

    Motor_driver_Software.ino
    Began work on main loop and speed control function

--- Motor Controller Software/Motor_driver_Software/Motor_driver_Software.ino
+++ Motor Controller Software/Motor_driver_Software/Motor_driver_Software.ino
@@ -8,13 +8,80 @@
 
 // test code
 
+#include <Servo.h>
+
+String inVal = "";
+char inDigit;
+int motorNumber, writeSpeed;
+int motorPin1 = 10, motorPin2 = 9, motorPin3 = 6, motorPin4 = 3;
+
+Servo esc1, esc2, esc3, esc4, choice;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  esc1.attach(motorPin1,1000,2000);                       // Attach servos to respective pins
+  esc2.attach(motorPin2,1000,2000);                       //
+  esc3.attach(motorPin3,1000,2000);                       //
+  esc4.attach(motorPin4,1000,2000);                       //
+
+  esc1.writeMicroseconds(1500);                           // Initialize motors at zero speed
+  esc2.writeMicroseconds(1500);                           //
+  esc3.writeMicroseconds(1500);                           //
+  esc4.writeMicroseconds(1500);                           //
+
+  delay(1000);                                            // Give motors time to initialize
+
+  Serial.print("Choose ESC to write to (1-4): ");         // Prompt user for ESC choice
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(Serial.available() == 0)                               // Take temperature and current readings while no input is available
+  {                                                         //
+    tempRead();                                             //
+                                                            //
+    currentRead();                                          //
+  }                                                         //
 
+  if(Serial.available() > 0)
+  {
+    inDigit = Serial.read();
+    motorNumber = inDigit.toInt();
+
+    switch(motorNumber)                                     // Choose which servo to write value to
+    {                                                       //
+      case 1:   choice = esc1                               //
+      case 2:   choice = esc2                               //
+      case 3:   choice = esc3                               //
+      case 4:   choice = esc4                               //
+    }
+    
+    motorWrite(choice);                                     // Enter motorWrite function
+  }
+
+  delay(10);
 }
+
+void tempRead()
+{
+  
+}
+
+void currentRead()
+{
+  
+}
+
+void motorWrite(Servo selectedMotor)
+{
+  Serial.print("Enter speed value in microseconds: ");
+  
+  while(Serial.available == 0)
+    delay(10);
+
+  while(Serial.available > 0)
+  {
+    
+  }
+}
+
