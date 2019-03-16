a19c1099a31070a6cf9eca6fad992c147700e5be
Yoga <yogamahartayasa@yahoo.com>
2019-03-15 19:44:19 -0700

    Update Motor_driver_Software.ino

--- Motor Controller Software/Motor_driver_Software/Motor_driver_Software.ino
+++ Motor Controller Software/Motor_driver_Software/Motor_driver_Software.ino
@@ -7,14 +7,27 @@
  */
 
 // test code
-
+int temp1 = A0;
+int temp2 = A1;
+const int tempControl = 100;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(temp1,INPUT);
+  pinMode(temp2,INPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int temp1_val = analogRead(temp1);
+  int temp2_val = analogRead(temp2);
+
+  if(temp1>=tempControl){
+    // power off
+  }
+  if(temp2>=100){
+    // shut off
+  }
 
 }
