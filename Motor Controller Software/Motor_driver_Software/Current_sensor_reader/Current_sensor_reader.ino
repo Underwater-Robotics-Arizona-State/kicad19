42a8299a9466bfa100a2d6a8051db95bccbff7ff
Yoga <yogamahartayasa@yahoo.com>
2019-03-26 20:05:43 -0700

    Create Current_sensor_reader.ino

---
+++ Motor Controller Software/Motor_driver_Software/Current_sensor_reader/Current_sensor_reader.ino
@@ -0,0 +1,34 @@
+int analogPin2 = A2;
+int analogPin3 = A3; 
+int analogPin4 = A4; 
+int analogPin5 = A5;
+
+int analogUse = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // When prompted to read current:
+  if(Serial.available() > 0){
+    escNum=Serial.read().toInt()
+     
+  }
+  
+}
+
+currentRead(int escNum){
+  switch(escNum){
+     case 2: analogUse = analogPinA2// esc 1
+     case 3: analogUse = analogPinA3// esc 2
+     case 4: analogUse = analogPinA4// esc 3
+     case 5: analogUse = analogPinA5// esc 4
+  }
+int voltageVal = analogRead(analogUse); // reads voltage input
+
+  
+}
