61c8d9e3f83b4d46e87860c06591d6e016415fa9
Yoga <yogamahartayasa@yahoo.com>
2019-04-12 19:27:15 -0700

    current

--- Motor Controller Software/Motor_driver_Software/Current_sensor_reader/Current_sensor_reader.ino
+++ Motor Controller Software/Motor_driver_Software/Current_sensor_reader/Current_sensor_reader.ino
@@ -2,8 +2,10 @@ int analogPin2 = A2;
 int analogPin3 = A3; 
 int analogPin4 = A4; 
 int analogPin5 = A5;
-
-int analogUse = 0;
+int escNum;
+int analogUse;
+float vRef;
+float sensitivity;
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,13 +24,14 @@ void loop() {
   
 }
 
-currentRead(int escNum){
+float currentRead(int escNum){
   switch(escNum){
      case 2: analogUse = analogPinA2// esc 1
      case 3: analogUse = analogPinA3// esc 2
      case 4: analogUse = analogPinA4// esc 3
      case 5: analogUse = analogPinA5// esc 4
   }
-  int voltageVal = analogRead(analogUse); // reads voltage input
+  float voltageVal = analogRead(analogUse); // reads voltage input
+  
   
 }
